8a9ec34d7daab7acfdc5f6e671990c80ea1efb34
daniel watkins <danielmwatkins@gmail.com>
2024-06-10 13:42:21 -0400

    adding notes to readme + bugfix

--- README.md
+++ README.md
@@ -11,4 +11,13 @@ TBD: Description of the MOSAiC buoy dataset
 
 Surface meteorological data was obtained from the ECMWF ERA5 reanalysis via the Copernicus Climate Data Store (CDS). The script `prepare_era_data.py` uses the `cdsapi` library to download hourly ERA5 data on a 0.25 degree latitude-longitude grid, then uses `xesmf` to regrid the data to a regular 25-km north polar stereographic grid (NSIDC North Polar Stereographic, EPSG code 3413).
 
-TBD: 
+TBD: The regrid step in `prepare_era_data` needs to be adjusted so that a single netCDF file is created rather than a separate file for each month and for each variable.
+
+
+# Task List
+* (Alan) Add description of MOSAiC buoy data and processing routine
+* (Alan) Downsample the MOSAiC buoy data to get daily positions for aligning with the SIC data
+* (Daniel) Add the NSIDC Sea Ice Concentration to the daily buoy positions (SIC data is only available at daily resolution)
+* Download the MASIE 1 km imagery for the periods where the buoys are getting close to ice edge to verify buoy end dates
+* Set up script to automatically check whether a buoy was in the 
+* Find subset of buoys where compass data is available -- perhaps on Pangaea for the SIMBA buoys?
--- scripts/prepare_era_data.py
+++ scripts/prepare_era_data.py
@@ -39,63 +39,59 @@ if download:
         saveloc = '../data/era5/'
         with urlopen(fl.location) as f:
             with xr.open_dataset(f.read()) as ds:
-                ds.to_netcdf(saveloc + savename + '_' + start_date.strftime('%Y%m%d') + '.nc',
-                             encoding={var: {'zlib': True}
-                                          for var in ds.variables()})
 
-if regrid:
-    # TBD: Refactor this code to (a) create a dataset with all the original variables still in it
-    # and (b) join all the monthly data.
+                # Set up 2d lat/lon grids
+                lon = ds.longitude
+                lat = ds.latitude
+                lon2d, lat2d = np.meshgrid(lon, lat)
+            
+                # Set resolution for output
+                dx = 25e3
+                xgrid = np.arange(-2e6, 2e6+1, dx)
+                xgrid, ygrid = np.meshgrid(xgrid, xgrid)
+                
+                proj_LL = 'epsg:4326' # WGS 84 Ellipsoid
+                proj_XY = 'epsg:3413' # NSIDC Polar Stereographic
+                transform_to_ll = pyproj.Transformer.from_crs(proj_XY, proj_LL, always_xy=True)
+                
+                longrid = np.zeros(xgrid.shape)
+                latgrid = np.zeros(xgrid.shape)
+                for ii in range(xgrid.shape[0]):
+                    for jj in range(xgrid.shape[1]):
+                        lonij, latij = transform_to_ll.transform(xgrid[ii, jj], ygrid[ii, jj])
+                        longrid[ii, jj] = lonij
+                        latgrid[ii, jj] = latij
 
-    # Open first file to get lat/lon grid
-    with xr.open_dataset(saveloc +  savename + '_' + '20191001' + '.nc') as ds_msl:
-        lon = ds_msl.longitude
-        lat = ds_msl.latitude
-        lon2d, lat2d = np.meshgrid(lon, lat)
-    
-    # Set resolution for output
-    dx = 25e3
-    xgrid = np.arange(-2e6, 2e6+1, dx)
-    xgrid, ygrid = np.meshgrid(xgrid, xgrid)
-    
-    proj_LL = 'epsg:4326' # WGS 84 Ellipsoid
-    proj_XY = 'epsg:3413' # NSIDC Polar Stereographic
-    transform_to_ll = pyproj.Transformer.from_crs(proj_XY, proj_LL, always_xy=True)
-    
-    longrid = np.zeros(xgrid.shape)
-    latgrid = np.zeros(xgrid.shape)
-    for ii in range(xgrid.shape[0]):
-        for jj in range(xgrid.shape[1]):
-            lonij, latij = transform_to_ll.transform(xgrid[ii, jj], ygrid[ii, jj])
-            longrid[ii, jj] = lonij
-            latgrid[ii, jj] = latij
-
-    for start_date in start_dates:
-        with xr.open_dataset(saveloc +  savename + '_' + '20191001' + '.nc') as ds: 
-            for var in ['msl', 'u10', 'v10']: # TBD: Get the short names for the cloud variables
-                attrs = ds.attrs
-                ds_in = xr.Dataset({var: (('time', 'xc', 'yc'), ds[var].data)},
-                                   coords={'time': (('time',), ds['time'].data),
-                                           'lon': (('xc', 'yc'), lon2d),
-                                           'lat': (('xc', 'yc'), lat2d)})
-        
-                ds_out = xr.Dataset(coords={'lon': (('xc', 'yc'), longrid),
-                                            'lat': (('xc', 'yc'), latgrid)})
-                regridder = xe.Regridder(ds_in, ds_out, "bilinear")
-                ds_regridded = regridder(ds_in)
-        
-                if var == 'msl':
-                    valid = ds_regridded['msl'] > 0
-                ds_regridded[var] = ds_regridded[var].where(valid)
-                ds_regridded = ds_regridded.assign_coords({'x_stere': (('xc', 'yc'), xgrid),
-                                        'y_stere': (('xc', 'yc'), ygrid)})
-                for x in ds_regridded.attrs:
-                    attrs[x] = ds_regridded.attrs[x]
-        
-                attrs['crs'] = proj_XY
-                ds_regridded.attrs = attrs
-                ds_regridded.rename({'lon': 'longitude', 'lat': 'latitude'})
-                # TBD: Add to a dictionary and then merge them before saving
-                ds_regridded.to_netcdf(saveloc_regridded + 'era5_' + var + '_regridded_' + savename + '.nc',
+                # carry out regridding variable-by-variable
+                regridded_data = []
+                for var in [v for v in ds.variables if v not in ['time', 'latitude', 'longitude']]:
+                    attrs = ds.attrs
+                    ds_in = xr.Dataset({var: (('time', 'xc', 'yc'), ds[var].data)},
+                                       coords={'time': (('time',), ds['time'].data),
+                                               'lon': (('xc', 'yc'), lon2d),
+                                               'lat': (('xc', 'yc'), lat2d)})
+            
+                    ds_out = xr.Dataset(coords={'lon': (('xc', 'yc'), longrid),
+                                                'lat': (('xc', 'yc'), latgrid)})
+                    regridder = xe.Regridder(ds_in, ds_out, "bilinear")
+                    ds_regridded = regridder(ds_in)
+            
+                    if var == 'msl':
+                        valid = ds_regridded['msl'] > 0
+                    ds_regridded[var] = ds_regridded[var].where(valid)
+                    ds_regridded = ds_regridded.assign_coords({'x_stere': (('xc', 'yc'), xgrid),
+                                            'y_stere': (('xc', 'yc'), ygrid)})
+                    for x in ds_regridded.attrs:
+                        attrs[x] = ds_regridded.attrs[x]
+                    attrs['crs'] = proj_XY
+                    ds_regridded.attrs = attrs
+                    regridded_data.append(ds_regridded.rename({'lon': 'longitude', 'lat': 'latitude'}))
+                    
+                    del ds_regridded
+                ds_regridded = xr.merge(regridded_data)
+                ds_regridded.attrs['history'] += ' Projected to 25 km NP Stereographic grid via xesmf'
+                ds_regridded.to_netcdf(saveloc + 'era5_regridded_' + start_date + '.nc',
                              encoding={v: {'zlib': True}
-                                          for v in [var, 'longitude', 'latitude']})
+                                          for v in ds_regridded.variables if v != 'time'})
+
+                del ds_regridded
